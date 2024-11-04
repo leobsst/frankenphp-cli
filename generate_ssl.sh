1882987139d036a0d4b23d011253eff3558ac81c
LEOBSST <contact@leobsst.fr>
2024-11-04 16:31:48 +0100

    fix

--- generate_ssl.sh
+++ generate_ssl.sh
@@ -12,18 +12,22 @@ fi
 # finally install certificates
 echo "-- Installing mkcert ..."
 sudo -u $USER mkcert -install
-echo "-- Creating and installing local SSL certificates for domain.s: ${DOMAINS} ..."
-
 sudo -u $USER mkcert -cert-file $CERTS_DIR/localhost.pem -key-file $CERTS_DIR/localhost-key.pem localhost
 
-for value in "${domains_list[@]}"; do
-    CERT_PEM_FILE="$CERTS_DIR/${value}.pem"
-    KEY_PEM_FILE="$CERTS_DIR/${value}-key.pem"
+if [[ "$APP_ENV" != "prod" ]] && [[ "$APP_ENV" != "production" ]]; then
+    echo "-- Creating and installing local SSL certificates for domain.s: ${DOMAINS} ..."
 
-    sudo -u $USER mkcert -cert-file ${CERT_PEM_FILE} -key-file ${KEY_PEM_FILE} ${value}
-done
+    sudo -u $USER mkcert -cert-file $CERTS_DIR/localhost.pem -key-file $CERTS_DIR/localhost-key.pem localhost
 
-echo
-echo "-- New SSL certificates generated!"
+    for value in "${domains_list[@]}"; do
+        CERT_PEM_FILE="$CERTS_DIR/${value}.pem"
+        KEY_PEM_FILE="$CERTS_DIR/${value}-key.pem"
+
+        sudo -u $USER mkcert -cert-file ${CERT_PEM_FILE} -key-file ${KEY_PEM_FILE} ${value}
+    done
+
+    echo
+    echo "-- New SSL certificates generated!"
+fi
 
 exit 0
